bb9a0936dadc639275988ac6d4ec1417cf7ad849
Toma <tomicawarr@gmail.com>
2019-07-13 20:45:43 +0200

    Restructured parts of the code

--- progress.js
+++ progress.js
@@ -6,14 +6,18 @@
     /**
      * @param {Number} value Total value of the progress bar
      * @param {Number} initialValue Value that the progress bar will start with. Automaticly calculated into percentages
+     * @param {Object} options You can pass additional options as an object, such as: ['color', 'animated']
      */
-    $.fn.createProgressBar = function(value = 100, initialValue = 0) 
+    $.fn.createProgressBar = function(value = 100, initialValue = 0, options = {}) 
     {
         progressBars[$(this).attr('id')] = {
             totalValue : value,
-            currentValue : initialValue
+            currentValue : initialValue,
+            color: (typeof options['color'] !== 'undefined') ? options['color'] : "red",
+            animated: (typeof options['animated'] !== 'animated') ? options['animated'] : false,
         };
         setProgressBarValue(this, initialValue);
+        setProgressBarColor(this, options['color']);
         return this;
     }
 
@@ -22,18 +26,30 @@
      */
     $.fn.setProgressBarColor = function(color)
     {
-        $(this).find(".__inner_progress").css({"background-color":color});
+        setProgressBarColor(this, color);
         return this;
     }
 
     /**
      * @param {Number} amount Setting value of the progress bar
-     * @param {bool} animated If you want the animation of progress bar filling or not. Default: false
+     * @param {Bool} animated If you want the animation of progress bar filling or not. Default: false
      */
     $.fn.setProgressBarValue = function(amount, animated = false)
     {
         setProgressBarValue(this, amount, animated);
         return this;
+    }
+
+    /**
+     * 
+     * @param {HTMLElement} element Element that will be manipulated
+     * @param {*} color Color that you want to set
+     */
+    function setProgressBarColor(element, color)
+    {
+        let activeBar = progressBars[$(element).attr('id')];
+        $(element).find(".__inner_progress").css({"background-color":color});
+        activeBar['color'] = color;
     }
 
     /**
@@ -45,12 +61,12 @@
     function setProgressBarValue(element, amount, animated = false)
     {
         let activeBar = progressBars[$(element).attr('id')];
-        activeBar['currentValue'] = amount;
         let percentage = (100 * amount) / activeBar['totalValue'];
-        
         let time = (2500*(percentage/100));
 
-        if(animated)
+        activeBar['currentValue'] = amount;
+
+        if(animated || activeBar['animated'])
         {
             $(element).find('.__inner_progress').animate({
                 "width": percentage+"%"
