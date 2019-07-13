d62aa1c671b7aa980d27839e9e044c577b4486cd
Toma <tomicawarr@gmail.com>
2019-07-13 18:07:38 +0200

    Setting & animating progress bar values

--- progress.js
+++ progress.js
@@ -13,7 +13,8 @@
             totalValue : value,
             currentValue : initialValue
         };
-        createVisualProgressBar(this);
+        setProgressBarValue(this, initialValue);
+        return this;
     }
 
     /**
@@ -22,18 +23,46 @@
     $.fn.setProgressBarColor = function(color)
     {
         $(this).find(".__inner_progress").css({"background-color":color});
+        return this;
+    }
+
+    /**
+     * @param {Number} amount Setting value of the progress bar
+     * @param {bool} animated If you want the animation of progress bar filling or not. Default: false
+     */
+    $.fn.setProgressBarValue = function(amount, animated = false)
+    {
+        setProgressBarValue(this, amount, animated);
     }
 
     /**
      * 
-     * @param {HTMLElement} element Class, ID or element itself that we will use to manipulate in HTML.
+     * @param {HTMLElement} element HTML emement that you want to manipulate
+     * @param {Number} amount New value of the progress bar
+     * @param {Bool} animated Set to true if you want animation while setting the progress bar
      */
-    function createVisualProgressBar(element)
+    function setProgressBarValue(element, amount, animated = false)
     {
         let activeBar = progressBars[$(element).attr('id')];
-        let percentage = (100 * activeBar['currentValue']) / activeBar['totalValue'];
-        $(element).find(".__inner_progress").width(percentage+"%");
+        activeBar['currentValue'] = amount;
+        let percentage = (100 * amount) / activeBar['totalValue'];
+        
+        let time = (2500*(percentage/100));
+
+        if(animated)
+        {
+            $(element).find('.__inner_progress').animate({
+                "width": percentage+"%"
+            }, time);
+        }
+        else 
+        {
+            $(element).find(".__inner_progress").width(percentage+"%");
+        }
     }
+
+
+
 
 
 }(jQuery));
