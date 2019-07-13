31b8f915ff69c03b8c51541db1eed9b57d7e2d2d
Toma <dijanic@gmail.com>
2019-07-13 17:35:01 +0200

    createPorgressBar logic & visual

--- progress.js
+++ progress.js
@@ -1,9 +1,31 @@
 (function($) {
 
-    var progressValue = 100;
+    var progressBars = [];
 
-    $.fn.setProgressBarValue = function(value) {
-        $(".__inner_progress").width(value);
+
+    /**
+     * @param {Number} value Total value of the progress bar
+     * @param {Number} initialValue Value that the progress bar will start with. Automaticly calculated into percentages
+     */
+    $.fn.createProgressBar = function(value = 100, initialValue = 0) 
+    {
+        progressBars[$(this).attr('id')] = {
+            totalValue : value,
+            currentValue : initialValue
+        };
+        createVisualProgressBar(this);
     }
+
+    /**
+     * 
+     * @param {HTMLElement} element Class, ID or element itself that we will use to manipulate in HTML.
+     */
+    function createVisualProgressBar(element)
+    {
+        let activeBar = progressBars[$(element).attr('id')];
+        let percentage = (100 * activeBar['currentValue']) / activeBar['totalValue'];
+        $(element).width(percentage+"%");
+    }
+
 
 }(jQuery));
